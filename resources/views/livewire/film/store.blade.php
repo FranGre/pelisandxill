dc08a7076aed58dc219559b80f10e7bfa97785c3
fran gregori <frangregori61@gmail.com>
2024-07-25 20:26:09 +0200

    implemented validation on store a film

--- resources/views/livewire/film/store.blade.php
+++ resources/views/livewire/film/store.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 use Illuminate\Support\Facades\Storage;
 use App\Models\TemporalyCover;
 use App\Models\Cover;
@@ -9,8 +9,19 @@ use App\Models\Film;
 
 state(['title', 'summary', 'sipnosis', 'year', 'director', 'duration', 'age', 'link']);
 
+rules([
+    'title' => 'required|string|max:60',
+    'summary' => 'required|string|max:500',
+    'sipnosis' => 'required|string|max:255',
+    'year' => 'required|integer|between:1900,2200',
+    'duration' => 'required|integer|between:0,400',
+    'director' => 'required|string|max:80',
+    'age' => 'required|integer|between:0,22',
+    'link' => 'required|url:https'
+]);
+
 $save = function () {
-    // validación
+    $this->validate();
 
     $temporalyCover = TemporalyCover::first();
 
@@ -46,48 +57,58 @@ $save = function () {
 
 <div>
     <h1 class="text-center">Nueva Película</h1>
-    <form wire:submit='save' class="space-y-12 mt-16">
+    <form wire:submit.prevent='save' class="space-y-12 mt-16">
         <div class="grid
         md:grid-cols-2 md:gap-6
         ">
             <div class="flex flex-col space-y-2">
                 <label>Title</label>
                 <x-input.text placeholder="Title..." wire:model='title' />
+                @error('title') <span class="text-redd-400">{{ $message }}</span> @enderror
+
             </div>
 
             <div class="flex flex-col space-y-2">
                 <label>Summary</label>
                 <x-input.text placeholder="Summary..." wire:model='summary' />
+                @error('summary') <span class="text-redd-400">{{ $message }}</span> @enderror
             </div>
 
             <div class="flex flex-col space-y-2">
                 <label>Sipnosis</label>
                 <x-input.text placeholder="Sipnosis..." wire:model='sipnosis' />
+                @error('sipnosis') <span class="text-redd-400">{{ $message }}</span> @enderror
             </div>
 
             <div class="flex flex-col space-y-2">
                 <label>Year</label>
                 <x-input.text placeholder="Year..." wire:model='year' />
+                @error('year') <span class="text-redd-400">{{ $message }}</span> @enderror
             </div>
 
             <div class="flex flex-col space-y-2">
                 <label>Director</label>
                 <x-input.text placeholder="Director..." wire:model='director' />
+                @error('director') <span class="text-redd-400">{{ $message }}</span> @enderror
             </div>
 
             <div class="flex flex-col space-y-2">
                 <label>Duration</label>
                 <x-input.text placeholder="Duration..." wire:model='duration' />
+                @error('duration') <span class="text-redd-400">{{ $message }}</span> @enderror
             </div>
 
             <div class="flex flex-col space-y-2">
                 <label>Age</label>
                 <x-input.text placeholder="Age..." wire:model='age' />
+                @error('age') <span class="text-redd-400">{{ $message }}</span> @enderror
+
             </div>
 
             <div class="flex flex-col space-y-2">
                 <label>Trailer</label>
                 <x-input.text placeholder="Trailer..." wire:model='link' />
+                @error('link') <span class="text-redd-400">{{ $message }}</span> @enderror
             </div>
         </div>
 
